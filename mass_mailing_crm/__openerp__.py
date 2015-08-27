7e94146a1075299fa5314a89306711badd7a501c
Anders Wallenquist <aw@leia.vertel.se>
2015-08-27 14:35:16 +0200

    Initial commit

---
+++ mass_mailing_crm/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2004-2015 Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Mass Mail CRM features',
+    'version': '1.0',
+    'category': 'Tools',
+    'description': """
+* Link directly to mass mail statistics from crm.lead
+====================================
+""",
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se',
+    'depends': ['mass_mailing','crm'],
+    'data': [
+        'crm_lead_view.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
