1301800410c7bdcc1c90b390fb1089080d30e5ed
Jakob Krabbe <jakob@syscare.se>
2019-10-09 16:27:32 +0200

    website_mass_mailing: [1927] Nyhetsbrev - Funktion för att läsa nyhetbrev när det inte fungerar i mailklienten

---
+++ website_mass_mailing/controller/main.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2004-2019 Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api, _
+from openerp.exceptions import except_orm, Warning, RedirectWarning
+from openerp import http
+from openerp.http import request
+from openerp import SUPERUSER_ID
+from datetime import datetime
+import werkzeug
+import pytz
+import re
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class website_massmailing(http.Controller):
+
+    @http.route(['/mass_mailing/<model("mail.mass_mailing"):mail>'], type='http', auth="public", website=True)
+    def view_mail(self, mail,**post):
+        return request.render('website_mass_mailing.mail', { 'mail':mail })
